5d43c8f25b88e6d213e6d6db905507f224691056
dwillcox <eugene.willcox@gmail.com>
2017-06-30 16:24:20 -0400

    Added a minimal example using cudaMallocPitch.

---
+++ cuda-malloc-pitch/cmpitch.f90
@@ -0,0 +1,137 @@
+module work_module
+
+  implicit none
+  
+contains
+  
+  attributes(global) subroutine work(x, coffset, n, xPitch, xLength)
+    integer, value, intent(in) :: coffset, n, xPitch, xLength
+    double precision, intent(inout) :: x(1:xPitch,1:xLength)
+    integer          :: i
+    
+    i = blockDim%x * (blockIdx%x - 1) + threadIdx%x
+    if (i > n) then
+       return
+    else
+       i = i + coffset
+       x(2, i) = x(1, i)
+    endif
+  end subroutine work
+  
+end module work_module
+
+
+program runstreams
+
+  use work_module
+  use cudafor
+
+  implicit none
+  
+  double precision, pinned, allocatable :: x(:,:)
+  double precision, allocatable, device :: xd(:,:)
+  double precision :: xsum
+  integer, parameter :: N = 1000000
+  integer, parameter :: M = 2
+  integer, parameter :: numStreams = 32
+  integer, parameter :: blockDim = 1024
+  logical :: pinnedflag
+  integer :: streamSize, istat, i, chunkOffset, chunkSize
+  integer :: xPitch
+  integer(kind=cuda_count_kind)  :: cuPitch, cuWidth, cuLength
+  integer(kind=cuda_stream_kind) :: streams(numStreams)
+  character(len=200) :: cudaErrorMessage
+  integer :: gridDim
+
+  ! Allocate data array in pinned memory and check
+  allocate(x(M,N), stat=istat, pinned=pinnedflag)
+  if (istat /= 0) then
+     write(*,*) 'Failed to allocate x array.'
+     stop
+  else
+     if (.not. pinnedflag) then
+        write(*,*) 'Allocated x array but failed to pin.'
+     endif
+  endif
+
+  ! Initialize host data array
+  x(1,:) = 1.0d0
+  x(2,:) = 0.0d0
+
+  ! Allocate data array in device
+  cuLength = N
+  cuWidth  = M
+  istat = cudaMallocPitch(xd, cuPitch, cuWidth, cuLength)
+  if (istat /= 0) then
+     cudaErrorMessage = cudaGetErrorString(istat)
+     write(*,*) 'Allocating Pitched Device Memory:'
+     write(*,*) cudaErrorMessage
+     write(*,*) 'cuPitch = ', cuPitch
+  end if
+
+  xPitch = cuPitch
+
+  ! Set cuda stream, block sizes
+  streamSize = ceiling(real(N)/numStreams)
+  gridDim    = ceiling(real(streamSize)/blockDim)
+
+  ! Create streams
+  do i = 1, numStreams
+     istat = cudaStreamCreate(streams(i))
+  enddo
+
+  ! Asynchronously copy chunks of x to device
+  do i = 1, numStreams
+     chunkOffset = (i-1) * streamSize
+     chunkSize  = min(streamSize, N-chunkOffset)
+     istat = cudaMemcpy2DAsync(xd(:, chunkOffset+1:), cuPitch, &
+                                x(:, chunkOffset+1:), cuWidth, cuWidth, &
+                                chunkSize, cudaMemcpyHostToDevice, streams(i))
+     if (istat /= 0) then
+        write(*,*) 'i = ', i
+        cudaErrorMessage = cudaGetErrorString(istat)
+        write(*,*) cudaErrorMessage
+     end if
+  enddo
+
+  ! Asynchronously work on chunks of x
+  do i = 1, numStreams
+     chunkOffset = (i-1) * streamSize
+     chunkSize  = min(streamSize, N-chunkOffset)     
+     call work<<<gridDim, blockDim, 0, streams(i)>>>(xd, chunkOffset, chunkSize, xPitch, N)
+  enddo
+
+  ! Asynchronously copy chunks of x back to host
+  do i = 1, numStreams
+     chunkOffset = (i-1) * streamSize
+     chunkSize  = min(streamSize, N-chunkOffset)
+     istat = cudaMemcpy2DAsync(x(:, chunkOffset+1:), cuWidth, &
+                               xd(:, chunkOffset+1:), cuPitch, cuWidth, &
+                               chunkSize, cudaMemcpyDeviceToHost, streams(i))
+     if (istat /= 0) then
+        write(*,*) 'i = ', i
+        cudaErrorMessage = cudaGetErrorString(istat)
+        write(*,*) cudaErrorMessage
+     end if
+  enddo
+  
+  ! Synchronize streams
+  do i = 1, numStreams
+     istat = cudaStreamSynchronize(streams(i))
+  enddo
+
+  ! Destroy streams
+  do i = 1, numStreams
+     istat = cudaStreamDestroy(streams(i))
+  enddo
+
+  ! Note that calling the intrinsic sum function
+  ! will give the wrong answer!
+  xsum = 0.0d0
+  do i = 1, N
+     xsum = xsum + x(1, i) + x(2, i)
+  end do
+
+  write(*,*) 'Sum of x is: ', xsum
+  
+end program runstreams
